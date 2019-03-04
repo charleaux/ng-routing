0e24a4acc63c7b75da02d8a2364dc843ce6d3754
miamidade\e314637 <charles.torry@miamidade.gov>
2019-03-04 16:56:50 -0500

    added many routes

--- src/app/servers/edit-server/edit-server.component.ts
+++ src/app/servers/edit-server/edit-server.component.ts
@@ -1,6 +1,8 @@
 import { Component, OnInit } from '@angular/core';
 
 import { ServersService } from '../servers.service';
+import { ActivatedRoute, Params } from '@angular/router';
+import { Subscription } from 'rxjs';
 
 @Component({
   selector: 'app-edit-server',
@@ -8,20 +10,42 @@ import { ServersService } from '../servers.service';
   styleUrls: ['./edit-server.component.css']
 })
 export class EditServerComponent implements OnInit {
-  server: {id: number, name: string, status: string};
+  server: { id: number; name: string; status: string };
   serverName = '';
   serverStatus = '';
+  paramsSubscription: Subscription;
+  allowEdit = false;
 
-  constructor(private serversService: ServersService) { }
+  constructor(
+    private serversService: ServersService,
+    private route: ActivatedRoute
+  ) {}
 
   ngOnInit() {
-    this.server = this.serversService.getServer(1);
+    console.log(this.route.snapshot.queryParams);
+    console.log(this.route.snapshot.fragment);
+    this.route.queryParams.subscribe((queryParams: Params) => {
+      console.log('queryParams: ', queryParams);
+      this.allowEdit = queryParams['allowEdit'] === '1' ? true : false;
+      console.log('this.allowEdit: ', this.allowEdit);
+    });
+    this.route.fragment.subscribe();
+
+    const id = +this.route.snapshot.params.id;
+
+    this.server = this.serversService.getServer(id);
     this.serverName = this.server.name;
     this.serverStatus = this.server.status;
+
+    this.paramsSubscription = this.route.params.subscribe((params: Params) => {
+      this.server = this.serversService.getServer(+params.id);
+    });
   }
 
   onUpdateServer() {
-    this.serversService.updateServer(this.server.id, {name: this.serverName, status: this.serverStatus});
+    this.serversService.updateServer(this.server.id, {
+      name: this.serverName,
+      status: this.serverStatus
+    });
   }
-
 }
